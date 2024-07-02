bbb390609e58397059e8b0de2f812c962fff7caf
savom <56549021+savom@users.noreply.github.com>
2024-07-02 17:20:21 +0900

    chapter1 요약
    ErrorShow 예제를 통해 FormatMessage 함수의 플래그의 역활과 사용방법 확인.

--- example/source/01-ErrorShow/ErrorShow.cpp
+++ example/source/01-ErrorShow/ErrorShow.cpp
@@ -54,24 +54,44 @@ void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
       break;
 
    case IDOK:
-      // Get the error code
+      // 에러 코드 획득(에디트 컨트롤로부터 에러코드를 숫자로 가져옴)
       DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
 
-      HLOCAL hlocal = NULL;   // Buffer that gets the error message string
+      HLOCAL hlocal = NULL;   // 에러 메시지 텍스트를 저장하기 위한 버퍼(메모리 블럭을 가리킬 핸들을 NULL로 초기화)
+      // FormatMessage 함수는 내부적으로 필요한 메모리 블록을 직접 할당하고 이를 가리키는 핸들을 반환할 수 있다.
 
-      // Use the default system locale since we look for Windows messages.
-      // Note: this MAKELANGID combination has 0 as value
+      // 윈도우 메시지 문자열을 얻기 위해 기본 시스템 지역 설정을 사용.
+      // 주의: 아래의 MAKELANGID는 0 값을 반환한다.
       DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
 
-      // Get the error code's textual description
+      // 에러 코드의 메시지 텍스트를 가져온다.
       BOOL fOk = FormatMessage(
          FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |
          FORMAT_MESSAGE_ALLOCATE_BUFFER, 
          NULL, dwError, systemLocale, 
          (PTSTR) &hlocal, 0, NULL);
+      /* FORMAT_MESSAGE_FROM_SYSTEM : ForMatMessage를 호출할 때 FORMAT_MESSAGE_FROM_SYSTEM 플래그를 전달할 수 있고,
+       이 플래그는 운영체제가 정의하고 있는 에러 코드와 대응되는 메시지 텍스트를 얻고자 한다는 것을 알리는 역활.*/
+
+      /* FORMAT_MESSAGE_ALLOCATE_BUFFER 플래그는 에러 메시지 텍스트를 저장할 수 있는 충분한 메모리 공간을 할당해 줄 것을 요청한다.
+      할당된 메모리 블록을 가리키는 핸들은 hlocal 변수를 통해 반환된다.*/
+
+      /* FORMAT_MESSAGE_IGNORE_INSERTS는 메시지 텍스트에 %로 시작하는 자리 표시자를 실질적인 값으로 변경하지 않을 것을 지정하는 플래그다.
+      자리 표시자는 상황에 맞는 추가 정보를 제공하기 위해 주로 사용된다. 
+      이 플래그를 인자로 전달하지 않으려면 자리 표시자에 나타날 정보를 Arguments 매개변수를 통해 전달해야 함.*/
+
+      /*dwError는 에러 코드를 전달.*/
+
+      /*systemLocale는 어떤 언어로 구성된 메시지 텍스트를 얻고자 하는지를 지정하는 언어 식별자를 전달하면 됨.
+      언어 식별자는 두 가지 상수 값을 기반으로 구성되는데, 수행될 운영체제의 기본 언어 설정을 가져 오기 위해 0을 지정할 수도 있음.
+      만약 어떤 언어의 운영체제가 설치되어 있는지를 미리 알 수 없다면 이 값을 특정 언어를 지정하도록 hard-coding 해서는 안된다.*/
+
+
+
+
 
       if (!fOk) {
-         // Is it a network-related error?
+         // 네트워크와 관련된 에러인가?
          HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL, 
             DONT_RESOLVE_DLL_REFERENCES);
 
